81e94caeec4c7154ebb7e318635b9ddfdeb01d35
MelvinWong96 <54987863+MelvinWong96@users.noreply.github.com>
2020-08-26 14:34:48 +0800

    Fixed an error in recieving message.

--- app/src/main/java/com/example/mdp/BluetoothChat.java
+++ app/src/main/java/com/example/mdp/BluetoothChat.java
@@ -16,7 +16,7 @@ import java.nio.charset.Charset;
 public class BluetoothChat {
 
     private static final String TAG = "BluetoothChat";
-    // private ConnectedThread myConnectedThread;
+
     private static Context myContext;
 
     private static BluetoothSocket mySocket;
@@ -90,9 +90,7 @@ public class BluetoothChat {
         }
     }
 
-    /*
-    //CALL THIS FROM MAIN ACTIVITY TO SEND DATA TO REMOTE DEVICE (ROBOT)//
-    */
+    //CALL THIS FROM MAIN ACTIVITY TO SEND DATA TO REMOTE DEVICE (ROBOT)
     public static void write(byte[] bytes) {
 
         String text = new String(bytes, Charset.defaultCharset());
@@ -116,31 +114,22 @@ public class BluetoothChat {
     }
 
 
-
     //METHOD TO START CHAT SERVICE
     static void connected(BluetoothSocket mySocket, BluetoothDevice myDevice, Context context) {
         Log.d(TAG, "Connected: Starting");
 
-        //showToast("Connection Established With: "+myDevice.getName());
         myBluetoothConnectionDevice = myDevice;
         myContext = context;
-        //Start thread to manage the connection and perform transmissions
         startChat(mySocket);
 
 
     }
 
-    /*
-    //WRITE TO CONNECTEDTHREAD IN AN UNSYNCHRONIZED MANNER
-    */
+
     public static void writeMsg(byte[] out) {
 
-        // Create temporary object
-        // ConnectedThread temp;
-
-        // Synchronize a copy of the ConnectedThread
         Log.d(TAG, "write: Write Called.");
-        //perform the write
+
         write(out);
 
     }
--- app/src/main/java/com/example/mdp/MainActivity.java
+++ app/src/main/java/com/example/mdp/MainActivity.java
@@ -1,22 +1,14 @@
 package com.example.mdp;
 
-import android.app.AlertDialog;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.util.UUID;
 
@@ -44,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements IMainActivity, Ma
         currentActivity = true;
 
         //REGISTER BROADCAST RECEIVER FOR INCOMING MSG
-        LocalBroadcastManager.getInstance(this).registerReceiver(btConnectionReceiver, new IntentFilter("btConnectionStatus"));
+       // LocalBroadcastManager.getInstance(this).registerReceiver(btConnectionReceiver, new IntentFilter("btConnectionStatus"));
         //LocalBroadcastManager.getInstance(this).registerReceiver(incomingMsgReceiver, new IntentFilter("IncomingMsg"));
 
         if (savedInstanceState == null) {
@@ -102,6 +94,7 @@ public class MainActivity extends AppCompatActivity implements IMainActivity, Ma
     }
 
     //BROADCAST RECEIVER FOR BLUETOOTH CONNECTION STATUS
+    /*
     BroadcastReceiver btConnectionReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -167,7 +160,7 @@ public class MainActivity extends AppCompatActivity implements IMainActivity, Ma
         }
     };
 
-
+*/
     private void doTopFragmentTransaction(Fragment fragment, String tag, boolean addToBackStack, String message){
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         if(!message.equals("")){
